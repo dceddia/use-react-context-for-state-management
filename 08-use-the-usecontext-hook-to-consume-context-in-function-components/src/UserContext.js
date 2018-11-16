8ce081563675ed0311aa1789821885dc2e8e685c
Dave Ceddia <dceddia@gmail.com>
2018-11-16 16:36:34 -0500

    Added Lesson 8

---
+++ 08-use-the-usecontext-hook-to-consume-context-in-function-components/src/UserContext.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FAKE_USER } from './api';
+
+let UserContext;
+const {
+  Provider,
+  Consumer
+} = (UserContext = React.createContext());
+// Context.Consumer, Context.Provider
+
+class UserProvider extends React.Component {
+  state = {
+    currentUser: FAKE_USER
+  };
+
+  handleLogin = user => {
+    this.setState({ currentUser: user });
+  };
+
+  handleLogout = () => {
+    this.setState({ currentUser: null });
+  };
+
+  render() {
+    return (
+      <Provider
+        value={{
+          user: this.state.currentUser,
+          onLogin: this.handleLogin,
+          onLogout: this.handleLogout
+        }}
+      >
+        {this.props.children}
+      </Provider>
+    );
+  }
+}
+
+export {
+  UserProvider,
+  Consumer as UserConsumer,
+  UserContext
+};
